64af5aae81b25bc4584cd4c6e96d8542fa2bbf81
shreykilaji <kilajishrey123@gmail.com>
2025-07-03 16:32:23 +0530

    Xpath concept

--- relativelocator/RelativeLocatorConcept.java
+++ relativelocator/RelativeLocatorConcept.java
@@ -23,41 +23,41 @@ public class RelativeLocatorConcept {
 	
 	public static void main(String[] args) {
 		
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver =new ChromeDriver();
+		 WebDriverManager.chromedriver().setup();
+		 WebDriver driver =new ChromeDriver();
 		
-		//launch browser->
-		driver.get("https://www.saucedemo.com/");
+		 //launch browser->
+		 driver.get("https://www.saucedemo.com/");
 		
-		//above()
-		By username = RelativeLocator.with(By.name("user-name")).above(By.name("password"));
-		driver.findElement(username).sendKeys("standard_user");
-		System.out.println("Username Entered successfully: PASS");
+		 //above()
+		 By username = RelativeLocator.with(By.name("user-name")).above(By.name("password"));
+		 driver.findElement(username).sendKeys("standard_user");
+		 System.out.println("Username Entered successfully: PASS");
 		
-		//below()
-		By password = RelativeLocator.with(By.name("password")).below(By.name("user-name"));
-		driver.findElement(password).sendKeys("secret_sauce");
-		System.out.println("Password Entered successfully: PASS");
+		 //below()
+		 By password = RelativeLocator.with(By.name("password")).below(By.name("user-name"));
+		 driver.findElement(password).sendKeys("secret_sauce");
+		 System.out.println("Password Entered successfully: PASS");
 		
 		/*
-		//toLeftOf()
-		By username =RelativeLocator.with(By.name("user-name")).toLeftOf(By.name("password"));
-		driver.findElement(username).sendKeys("standard_user");
+		 //toLeftOf()
+		 By username =RelativeLocator.with(By.name("user-name")).toLeftOf(By.name("password"));
+		 driver.findElement(username).sendKeys("standard_user");
 		
-		//toRightOf()
-		By password =RelativeLocator.with(By.name("password")).toRightOf(By.name("user-name"));
-		driver.findElement(password).sendKeys("secret_sauce");
-		
+		 //toRightOf()
+		 By password =RelativeLocator.with(By.name("password")).toRightOf(By.name("user-name"));
+		 driver.findElement(password).sendKeys("secret_sauce");
 		
 		 //near()->clicking on login button using near relative locator
 		 By loginbutton = RelativeLocator.with(By.id("login-button")).near(By.name(""));
 		 driver.findElement(loginbutton).click();
 		 System.out.println("Login button clicked successfully: PASS");
-		 */
+		 
+		*/
 		
-		WebElement loginbutton = driver.findElement(By.id("login-button"));
-		loginbutton.click();
-		System.out.println("Login button clicked successfully: PASS");
+		 WebElement loginbutton = driver.findElement(By.id("login-button"));
+		 loginbutton.click();
+		 System.out.println("Login button clicked successfully: PASS");
 	}
 
 }
---
+++ xapthconcept/XpathConcept.java
@@ -0,0 +1,40 @@
+package xapthconcept;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+//Two types of xpath-->
+//1. Absolute Xpath(contains full path from root to desired element)
+
+//2. Relative Xpath (Majorly used instead of absolute xpath)->
+//   it starts with (//)
+
+
+public class XpathConcept {
+	
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		//launch Chrome->
+		driver.get("https://www.saucedemo.com/");
+		System.out.println("Chrome browser launched successfully");
+		
+		//enter user-name using xpath
+		driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys("standard_user");
+		System.out.println("Username entered successfully");
+		
+		//enter password using xpath
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("secret_sauce");
+		System.out.println("Password entered successfully");
+		
+		//clicking submit button using xpath
+		driver.findElement(By.xpath("//input[@id='login-button']")).click();
+		System.out.println("Successfully clicked on login button.");
+		
+	}
+
+}
